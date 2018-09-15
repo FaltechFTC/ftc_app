09f7beed7e5ada7b792fc433c23ddb18a9debde9
Error87C <johnmcatx05@gmail.com>
2018-09-15 13:55:58 -0500

    Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FaltechLinearExtensionTeleop.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+public class FaltechLinearExtensionTeleop {
+    /**
+     *Created by Error87C on 9/6/18
+     */
+if (rightTrigger > 0.2 && rightTrigger < 0.5){
+
+        robot.mtrCollector.setPower(0.5);
+
+    } else if (rightTrigger > 0.5){
+
+        robot.mtrCollector.setPower(1.0);
+
+        { else
+
+            robot.mtrCollector.setPower(0.0);
+
+        }
+
+    }
+    //Test
