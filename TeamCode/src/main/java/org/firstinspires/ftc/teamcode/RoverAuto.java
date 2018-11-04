0e42d2d5db1c672fea721e92ac878c0160874fe8
John <ted.gaunt@lucidtechnique.com>
2018-11-04 11:59:49 -0600

    cleaned up files (deleted unneeded ones), merged in configuration to auto, and merged in button press utility

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverAuto.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.RobotLog;
 
 @Autonomous(name="RoverAuto", group="7079")
 public class RoverAuto extends LinearOpMode {
@@ -52,53 +52,81 @@ public class RoverAuto extends LinearOpMode {
     double maxPowerAuto = 0.5;
     double maxTurningPower = 0.1;
     double degreesError =3;
-    int goldPosition = 2;
+    int goldPosition = 1;
     double goldDegrees = 5;
     double knockOffDistance =24;
 
     @Override
     public void runOpMode() {
-        telemetry.addData("About to start", "Robot Initialization");
+        telemetry.addData("Status", "Begin Robot Initialization");
         telemetry.update();
         robot = new RoverRobot(new DriveMecanum());
 
 
-        robot.init(hardwareMap, telemetry,true);
-        telemetry.addData("Robot" , "Initialized");
+        robot.init(hardwareMap, telemetry, true);
 
-     //   configMode();
+        telemetry.addData("Status", "Robot Initialized");
+        telemetry.update();
+
+        configMode();
+
+        telemetry.addData("Status", "Configured. Waiting for Start");
+        telemetry.update();
 
         waitForStart();
 
+        telemetry.addData("Status", "Running Autonomous!");
+        telemetry.update();
 
+
+        doLander();
+        doMinerals();
+        doDepot();
+        doCrater();
+    }
+
+    public void doLander() {
         // Rover Lift Code
         // TODO : Check if there is need to go up before going down, if so we need to add that code
-       if (isStartLatched) {
-           robot.roverLift.setTargetPosition(4);
-           robot.roverLift.setPower(0.4);
+        if (isStartLatched) {
+            robot.roverLift.setTargetPosition(4);
+            robot.roverLift.setPower(0.4);
 
-           // Strafe right for 300 milliseconds
+            // Strafe right for 300 milliseconds
 
-           robot.drive.driveFRS(0, 0, 1, maxPowerAuto);
-           sleep(300);
-       }
+            robot.drive.driveFRS(0, 0, 1, maxPowerAuto);
+            sleep(300);
+        }
         // TODO: We need to check for which spot is the mineral in
 
-        if (goldPosition==1) {
-           goldDegrees = 30;
-           knockOffDistance = 28;
-        } else if (goldPosition==2 ) {
-           goldDegrees = 5;
-           knockOffDistance = 60;
+    }
+
+    public void doMinerals() {
+        if (goldPosition == 1) {
+            goldDegrees = 30;
+            knockOffDistance = 28;
+        } else if (goldPosition == 2) {
+            goldDegrees = 5;
+            knockOffDistance = 60;
         } else {
-           goldDegrees = -30;
-           knockOffDistance = 28;
+            goldDegrees = -30;
+            knockOffDistance = 28;
         }
         robot.drive.setRunModeEncoder(false);
-        operation = robot.getOperationDriveToHeading(goldDegrees,maxPowerAuto,maxTurningPower,degreesError,10000,knockOffDistance);
+        operation = robot.getOperationDriveToHeading(goldDegrees, maxPowerAuto, maxTurningPower, degreesError, 10000, knockOffDistance);
         operation.run();
         robot.stop();
+    }
 
+    public void doDepot() {
+
+    }
+
+    public void doCrater() {
+
+    }
+
+    public void doParkingLot() {
 
 //        // Need to drive back 12 inches to start the turn
 //
@@ -157,31 +185,66 @@ public class RoverAuto extends LinearOpMode {
 //        operation.run();
 //        robot.stop();
 
-
-
-
     }
 
     void configMode() {
-        while (!gamepad1.start) {
-            if (gamepad1.x) isRedAlliance= !isRedAlliance;
-            if (gamepad1.y) isStartFacingCrater= !isStartFacingCrater;
-            if (gamepad1.a) isEnableCV= !isEnableCV;
-            if (gamepad1.b) isStartLatched= !isStartLatched;
+        String lastModes="";
 
-            // just a test...  to be reworked...  can we do this at start?
+        RobotLog.i("configMode() start");
+        telemetry.addData("ConfigMode" , "Press right bumper to leave config mode.");
+        telemetry.update();
+
+        do {
+
+            if (FaltechUtilities.isValueChangedAndEqualTo("1.x",gamepad1.x,true))
+                isRedAlliance= !isRedAlliance;
+            if (FaltechUtilities.isValueChangedAndEqualTo("1.y",gamepad1.y,true))
+                isStartFacingCrater= !isStartFacingCrater;
+            if (FaltechUtilities.isValueChangedAndEqualTo("1.a",gamepad1.a,true))
+                isEnableCV= !isEnableCV;
+            if (FaltechUtilities.isValueChangedAndEqualTo("1.b",gamepad1.b,true))
+                isStartLatched= !isStartLatched;
+
+
+            if (FaltechUtilities.isValueChangedAndEqualTo("1.dd",gamepad1.dpad_down,true)) {
+                goldPosition++;
+                if (goldPosition > 3) goldPosition = 1;
+
+            }
+
+/*            // just a test...  to be reworked...  can we do this at start?
             double liftPower=FaltechUtilities.clipDeadzone(gamepad1.right_trigger-gamepad1.left_trigger,.2);
             if (liftPower>0) robot.roverLift.mtrRoverLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // TODO: remove this...
             robot.roverLift.setPower(liftPower);
+*/
 
-            telemetry.addData("Alliance", isRedAlliance?"Red":"Blue");
-            telemetry.addData("Facing", isStartFacingCrater?"Crater":"Depot");
-            telemetry.addData("CV", isEnableCV?"Enabled":"Disabled");
-            telemetry.addData("Hanging", isStartLatched?"Latched":"On The Ground");
-            telemetry.addData("ConfigMode" , "Press START to leave config mode.");
-            telemetry.update();
+            String modes="";
+            modes+="Alliance="+(isRedAlliance?"Red":"Blue");
+            modes+=", Facing="+(isStartFacingCrater?"Crater":"Depot");
+            modes+=", CV="+(isEnableCV?"Enabled":"Disabled");
+            modes+=", Hanging="+(isStartLatched?"Latched":"On The Ground");
+            modes+=", Gold = "+goldPosition;
+
+            if (!modes.equals(lastModes)) {
+                telemetry.addData("Alliance", isRedAlliance?"Red":"Blue");
+                telemetry.addData("Facing", isStartFacingCrater?"Crater":"Depot");
+                telemetry.addData("CV", isEnableCV?"Enabled":"Disabled");
+                telemetry.addData("Hanging", isStartLatched?"Latched":"On The Ground");
+                telemetry.addData("Gold Position = ", goldPosition);
+                telemetry.addData("ConfigMode" , "Press right bumper to leave config mode.");
+                telemetry.update();
+
+                RobotLog.i(modes);
+                lastModes=modes;
+            }
+
             sleep(100);
-        }
+        } while (!gamepad1.right_bumper);
+
+        telemetry.addData("ConfigMode" , lastModes);
+        telemetry.update();
+
+        RobotLog.i("configMode() stop");
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverTestImu.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverTestImu.java
@@ -33,9 +33,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.Func;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
@@ -56,7 +54,7 @@ import java.util.Locale;
  *
  * @see <a href="http://www.adafruit.com/products/2472">Adafruit IMU</a>
  */
-@Autonomous(name = "Sensor: BNO055 IMU", group = "7079")
+@Autonomous(name = "TEST IMU", group = "7079")
                            // Comment this out to add to the opmode list
 public class RoverTestImu extends LinearOpMode
 {
