8801b8229c3f978a3615a7d512e1ce580f4debd9
Ted Gaunt <ted.gaunt@lucidtechnique.com>
2018-11-08 11:17:56 -0600

    helping by moving vision stuff to a separate class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverAuto.java
@@ -2,17 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.RobotLog;
-
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
-import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
-
-import java.util.List;
 
 /* all TensorFlow originally from examples provided by First from class ConceptTensorFlowObjectDetection.java */
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverVision.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -9,6 +8,8 @@ import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
 import java.util.List;
 
@@ -43,52 +44,51 @@ public class RoverVision {
         Recognition bestGold=null; // use this to track the best gold that we find.
 
         while ((opMode==null || opMode.opModeIsActive()) && runTime.milliseconds()<maxMilliseconds) {
+            sleep(400); // give time to find stuff
+
             // getUpdatedRecognitions() will return null if no new information is available since
             // the last time that call was made.
             List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
             int size=0;
-            if (updatedRecognitions==null) {
-                ; // DO NOTHING
-            } else {
-                size=updatedRecognitions.size();
-                int objNum=0;
-                // loop through all of the found objects
-                for (Recognition obj : updatedRecognitions) {
+            if (updatedRecognitions==null) continue;
 
-                    // for debugging, output all that we find.
-                    telemetry.addData("Vision Obj#"+ objNum++, "label="+obj.getLabel()+
-                            " left="+obj.getLeft()+
-                            " right="+obj.getRight()+
-                            " top="+obj.getTop()+
-                            " confidence="+obj.getConfidence());
+            size=updatedRecognitions.size();
+            int objNum=0;
+            // loop through all of the found objects
+            for (Recognition obj : updatedRecognitions) {
 
-                    // if we found gold
-                    if (obj.getLabel().equals(LABEL_GOLD_MINERAL)) {
-                        // if it isn't too high (in crater) and if it has good confidence
-                        if (obj.getTop() < 600 && obj.getConfidence() > 50) {
-                            // track this one as the best, if we don't have one yet
-                            if (bestGold == null)
-                                bestGold = obj;
-                                // or if the one just found is better than the one we found before
-                            else if (obj.getConfidence() > bestGold.getConfidence())
-                                bestGold = obj;
-                            else {
-                                // just going to keep existing bestGold as it has higher confidence
-                            }
+                // for debugging, output all that we find.
+                telemetry.addData("Vision Obj#"+ objNum++, "label="+obj.getLabel()+
+                        " left="+obj.getLeft()+
+                        " right="+obj.getRight()+
+                        " top="+obj.getTop()+
+                        " confidence="+obj.getConfidence());
+
+                // if we found gold
+                if (obj.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                    // if it isn't too high (in crater) and if it has good confidence
+                    if (obj.getTop() < 600 && obj.getConfidence() > 50) {
+                        // track this one as the best, if we don't have one yet
+                        if (bestGold == null)
+                            bestGold = obj;
+                            // or if the one just found is better than the one we found before
+                        else if (obj.getConfidence() > bestGold.getConfidence())
+                            bestGold = obj;
+                        else {
+                            // just going to keep existing bestGold as it has higher confidence
                         }
                     }
-
                 }
+
             }
             telemetry.addData("Vision", "objects="+size+"  time elapse="+runTime.seconds());
-            if (recognition!=null)
+            if (bestGold!=null)
                 telemetry.addData("Best Gold",
-                        " left="+recognition.getLeft()+
-                                " right="+recognition.getRight()+
-                                " top="+recognition.getTop()+
-                                " confidence="+recognition.getConfidence());
+                        " left="+bestGold.getLeft()+
+                                " right="+bestGold.getRight()+
+                                " top="+bestGold.getTop()+
+                                " confidence="+bestGold.getConfidence());
             telemetry.update();
-            sleep(400);
         }
 
         tfod.shutdown();
@@ -167,5 +167,11 @@ public class RoverVision {
         telemetry.update();
     }
 
-
+    private void sleep(long millis) {
+        try {
+            java.lang.Thread.sleep(400);
+        } catch (InterruptedException e) {
+            ; // do nothing
+        }
+    }
 }
