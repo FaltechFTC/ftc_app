1739617868ff6757c3ffa6902156e55251857756
John <ted.gaunt@lucidtechnique.com>
2019-01-02 10:08:59 -0600

    drivetodistance tweak

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpDriveToDistance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpDriveToDistance.java
@@ -6,18 +6,25 @@ import com.qualcomm.robotcore.util.RobotLog;
 public class OpDriveToDistance extends Operation {
 
 
-    double maxDrivePower;
+    double maxDrivePower, maxRotatePower;
     double targetDistance;
     double targetTolerance;
     double startingEncoder, targetEncoder;
 
-    Pid pidDrive;
+    Pid pidDrive, pidRotate;
 
     double curEncoder=0;
+    public int onTargetLoopCount=0, maxTargetLoopCount=3;
+
 
     public OpDriveToDistance(RoverRobot robot, double maxDrivePower, long timeoutMS, double targetDistance, double targetTolerance) {
+        this(robot,maxDrivePower,.06,timeoutMS,targetDistance,targetTolerance);
+    }
+
+    public OpDriveToDistance(RoverRobot robot, double maxDrivePower, double maxRotatePower, long timeoutMS, double targetDistance, double targetTolerance) {
         super(robot);
         this.maxDrivePower=maxDrivePower;
+        this.maxRotatePower=maxRotatePower;
         this.timeoutMS=timeoutMS;
         this.targetDistance=targetDistance;
 
@@ -25,10 +32,13 @@ public class OpDriveToDistance extends Operation {
         coastOnStop = false;
 
         pidDrive = new Pid(.6, 0.01, 0.2, -100, 100, -maxDrivePower, maxDrivePower);
+        pidRotate = new Pid(.5, 0.04, 0.3, -20, 20, -maxRotatePower, maxRotatePower);
 
         startingEncoder= robot.drive.getEncoderClicksFront();
         targetEncoder=startingEncoder+robot.drive.convertInchesToClicks(targetDistance);
         this.targetTolerance=Math.abs(robot.drive.convertInchesToClicks(targetTolerance));
+
+        robot.resetRelativeAngleToZero();
     }
 
     @Override
@@ -49,11 +59,22 @@ public class OpDriveToDistance extends Operation {
         RobotLog.i("DriveToDistance: start=%f  target=%f  current=%f", startingEncoder, targetEncoder, curEncoder);
 
         if (Math.abs(targetEncoder - curEncoder)<= targetTolerance) {
+            onTargetLoopCount++;
+        } else {
+            onTargetLoopCount = 0;
+        }
+
+        if (onTargetLoopCount>=maxTargetLoopCount) {
             done();
         } else {
+            double relativeAngle = -robot.getRelativeAngle();
+            double rotatePower = pidRotate.update(/*desired*/0, /*actual*/relativeAngle, deltaTime);
             double drivePower = pidDrive.update(/*desired*/targetEncoder, /*actual*/curEncoder, deltaTime);
-            robot.drive.driveFRS(drivePower, 0, 0);
-            robot.telemetry.addData("forwardPower", drivePower);
+            robot.drive.driveFRS(drivePower, rotatePower, 0);
+            String s=String.format("forward=%3.2f  rotate=%3.2f",drivePower, rotatePower);
+            robot.telemetry.addData("DriveToHeading Powers", s);
+            RobotLog.i("DriveToHeading Powers "+s);
+
         }
         return !done;
     }
